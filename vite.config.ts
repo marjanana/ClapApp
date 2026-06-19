89247ee6e62c2b57dbe1fe6f40aba82195de072f
anamy <anamy@users.noreply.github.com>
2026-06-19 16:29:31 -0400

    Configure app for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/ClapApp/" : "/",
   plugins: [react()],
   test: {
     environment: "jsdom",
@@ -9,4 +10,4 @@ export default defineConfig({
     setupFiles: "./src/test/setup.ts",
     css: true
   }
-});
+}));
